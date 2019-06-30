64d7f6090122845554f7f6239275813d79f040c8
Pranit-Harekar <pranitharekar@hotmail.com>
2019-06-30 16:28:42 -0400

    Routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import '../styles/App.css'
 import LinkList from './LinkList'
+import CreateLink from './CreateLink'
+import Header from './Header'
+import { Switch, Route } from 'react-router'
 
-const App: React.FC = () => <LinkList />
+const App: React.FC = () => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Switch>
+        <Route exact path="/" component={LinkList} />
+        <Route exact path="/create" component={CreateLink} />
+      </Switch>
+    </div>
+  </div>
+)
 
 export default App
